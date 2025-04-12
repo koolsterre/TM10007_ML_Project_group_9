d714365763d294a15d26fc1e30e4c140dc297f18
Sofie Stelder <S.A.Stelder@student.tudelft.nl>
2025-04-12 16:04:51 +0200

    Changed the outlier detection of test_data so it uses the threshold of train_data

--- outliers.py
+++ outliers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import zscore
+import pandas as pd
 
 def outlier_detection(data):
     drempel = 3
@@ -17,3 +18,30 @@ def outlier_detection(data):
         total_outliers += outliers.sum()
 
     return data, total_outliers
+
+
+def outlier_detection_test(data_train, data_test):
+    drempel = 3
+    total_outliers_test = 0
+    for column in data_train.select_dtypes(include=[np.number]).columns:
+
+        mean_value = data_train[column].mean()
+        std_value = data_train[column].std()
+    
+        z_scores = list()
+        for i in data_test.index:
+            data_value = data_test.loc[i,column]
+            if std_value == 0:
+                z_score = 0
+            else:
+                z_score = abs((data_value - mean_value)/std_value)
+            z_scores.append(z_score)
+
+        z_scores_series = pd.Series(z_scores,index=data_test.index) #Create a Series with the z-scores, using the mean and STD of train_data
+        outliers = z_scores_series > drempel #Detecting outliers
+
+    # Vervang de outliers met de grenswaarden
+        data_test.loc[outliers,column] = np.clip(data_test.loc[outliers,column], mean_value - drempel * std_value, mean_value + drempel * std_value)
+        total_outliers_test += outliers.sum()
+
+    return data_test, total_outliers_test

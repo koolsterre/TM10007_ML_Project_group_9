4ec8fbadee0346b4059604f0f3e9c5dc847a348e
elkedelaat <e.w.j.delaat@student.tudelft.nl>
2025-04-18 09:46:10 +0200

    Extra spaties weg

--- outliers.py
+++ outliers.py
@@ -1,61 +1,61 @@
 def replace_outliers(data_train):
-     # Alleen numerieke kolommen
-     numeric_cols = data_train.select_dtypes(include=['number'])
-     total_capped = 0
-     # Pas outliers direct aan in df
-     for col in numeric_cols.columns:
-         data = data_train[col].dropna()
+    # Alleen numerieke kolommen
+    numeric_cols = data_train.select_dtypes(include=['number'])
+    total_capped = 0
+    # Pas outliers direct aan in df
+    for col in numeric_cols.columns:
+        data = data_train[col].dropna()
  
-         Q1 = data.quantile(0.25)
-         Q3 = data.quantile(0.75)
-         IQR = Q3 - Q1
+        Q1 = data.quantile(0.25)
+        Q3 = data.quantile(0.75)
+        IQR = Q3 - Q1
  
-         lower_bound = Q1 - 3 * IQR
-         upper_bound = Q3 + 3 * IQR
+        lower_bound = Q1 - 3 * IQR
+        upper_bound = Q3 + 3 * IQR
+ 
+        # Boolean mask voor outliers
+        lower_outliers = data_train[col] < lower_bound
+        upper_outliers = data_train[col] > upper_bound
+        capped_count = lower_outliers.sum() + upper_outliers.sum()
+ 
+        # Update total
+        total_capped += capped_count
+ 
+        # Winsorize in-place met .apply()
+        data_train[col] = data_train[col].apply(lambda x: 
+            lower_bound if x < lower_bound else
+            upper_bound if x > upper_bound else
+            x
+        )
+    return data_train, total_capped, lower_bound, upper_bound
+ 
+def replace_outliers_test(data_train, data_test, lower_bound, upper_bound):
+    # Alleen numerieke kolommen
+    numeric_cols = data_test.select_dtypes(include=['number'])
+    total_capped_test = 0
+    # Pas outliers direct aan in df
+    for col in numeric_cols.columns:
+        data = data_train[col].dropna()
+ 
+        Q1 = data.quantile(0.25)
+        Q3 = data.quantile(0.75)
+        IQR = Q3 - Q1
+ 
+        lower_bound = Q1 - 3 * IQR
+        upper_bound = Q3 + 3 * IQR
  
          # Boolean mask voor outliers
-         lower_outliers = data_train[col] < lower_bound
-         upper_outliers = data_train[col] > upper_bound
-         capped_count = lower_outliers.sum() + upper_outliers.sum()
+        lower_outliers = data_test[col] < lower_bound
+        upper_outliers = data_test[col] > upper_bound
+        capped_count = lower_outliers.sum() + upper_outliers.sum()
  
-         # Update total
-         total_capped += capped_count
+        # Update total
+        total_capped_test += capped_count
  
-         # Winsorize in-place met .apply()
-         data_train[col] = data_train[col].apply(lambda x: 
-             lower_bound if x < lower_bound else
-             upper_bound if x > upper_bound else
-             x
-         )
-     return data_train, total_capped, lower_bound, upper_bound
- 
- def replace_outliers_test(data_train, data_test, lower_bound, upper_bound):
-     # Alleen numerieke kolommen
-     numeric_cols = data_test.select_dtypes(include=['number'])
-     total_capped_test = 0
-     # Pas outliers direct aan in df
-     for col in numeric_cols.columns:
-         data = data_train[col].dropna()
- 
-         Q1 = data.quantile(0.25)
-         Q3 = data.quantile(0.75)
-         IQR = Q3 - Q1
- 
-         lower_bound = Q1 - 3 * IQR
-         upper_bound = Q3 + 3 * IQR
- 
-         # Boolean mask voor outliers
-         lower_outliers = data_test[col] < lower_bound
-         upper_outliers = data_test[col] > upper_bound
-         capped_count = lower_outliers.sum() + upper_outliers.sum()
- 
-         # Update total
-         total_capped_test += capped_count
- 
-         # Winsorize in-place met .apply()
-         data_test[col] = data_test[col].apply(lambda x: 
-             lower_bound if x < lower_bound else
-             upper_bound if x > upper_bound else
-             x
-         )
-     return data_test, total_capped_test
+        # Winsorize in-place met .apply()
+        data_test[col] = data_test[col].apply(lambda x: 
+            lower_bound if x < lower_bound else
+            upper_bound if x > upper_bound else
+            x
+        )
+    return data_test, total_capped_test
